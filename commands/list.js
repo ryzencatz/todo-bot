191734c07024454b35c43ba26ecf3b151f903e7f
ryzencatz <ryzencatz@gmail.com>
2026-06-16 19:27:27 -0700

    fixed some stuff in list.js and added stuff for show.js

--- commands/list.js
+++ commands/list.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require("discord.js");
 const { loadData, saveData } = require("../data/storage");
 
 module.exports = {
-    data = new SlashCommandBuilder()
+    data: new SlashCommandBuilder()
         .setName("list")
         .setDescription("does stuff related to lists")
         .addSubcommand(subcommand => 
@@ -11,17 +11,21 @@ module.exports = {
             .setDescription("creates a new list")
             .addStringOption(option => 
                 option
-                .setName("list name")
+                .setName("list-name")
                 .setDescription("adds a list name to identify the list")
                 .setRequired(true)
             )
         ),
-        async execute(interaction) {
-            const data = loadData();
+    async execute(interaction) {
+        const data = loadData();
 
-            const listname = interaction.options.getString("list name");
+        if (interaction.options.getSubcommand === "create") {
+            const listname = interaction.options.getString("list-name");
             data[listname] = [];
 
             saveData(data);
+
+            await interaction.reply(`created a new list called ${listname}!`);
         }
-}
+    },
+};
---
+++ commands/show.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder } = require("discord.js");
+const { loadData, saveData } = require("../data/storage");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("show")
+        .setDescription("shows the list")
+        .addStringOption(option => option.setName("list-name").setDescription("the list you want to show")),
+    async execute(interaction) {
+        const data = loadData();
+
+        const list = interaction.options.getString("list-name");
+        let incomplete = ``;
+        let complete = ``;
+
+        data[list].forEach((item, index) => {
+            if (item.completed) {
+                complete +=
+                    `${index + 1}. ~~${item.content}~~\n`;
+            } else {
+                incomplete +=
+                    `${index + 1}. ${item.content}\n`;
+            }
+        });
+        
+        await interaction.reply(`\n**${list}**\n${complete}${incomplete}`);
+    }
+};
